81d9a2679a34ba01a04d71a6e29b9f284ecb3d83
Micael Levi (@micalevisk) <mllc@icomp.ufam.edu.br>
2022-02-01 14:21:12 -0400

    refactor: minor migrations to modern javascript
    What was done:
    - use camel case
    - use arrow functions
    - add JSDocs to most functions
    - use return early pattern (whenever make sense)
    - more cohesive `ssh_keygen` function

--- src/ssh-keygen.js
+++ src/ssh-keygen.js
@@ -1,12 +1,13 @@
-var spawn = require('child_process').spawn;
-var fs = require('fs');
-var os = require('os');
-var path = require('path');
-var _ = require('./utils');
+const spawn = require('child_process').spawn;
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const _ = require('./utils');
 
-var log = function (a) {
+function log(a) {
   if (process.env.VERBOSE) console.log('ssh-keygen: ' + a);
-};
+}
+
 function binPath() {
   if (process.platform !== 'win32') return 'ssh-keygen';
 
@@ -19,25 +20,38 @@ function binPath() {
 
   throw new Error('Unsupported platform');
 }
+
+/**
+ *
+ * @param {string} location
+ * @param {boolean} force
+ * @param {(err?: any) => void} callback
+ */
 function checkAvailability(location, force, callback) {
-  var pubLocation = location + '.pub';
+  const pubLocation = location + '.pub';
   log('checking availability: ' + location);
 
-  fs.access(location, fs.constants.R_OK | fs.constants.W_OK, function (keyAccessErr) {
+  fs.access(location, fs.constants.R_OK | fs.constants.W_OK, (keyAccessErr) => {
     log('checking availability: ' + pubLocation);
-    var keyExists = !keyAccessErr;
-    fs.access(location, fs.constants.R_OK | fs.constants.W_OK, function (pubKeyAccessErr) {
-      var pubKeyExists = !pubKeyAccessErr;
+    const keyExists = !keyAccessErr;
+    fs.access(location, fs.constants.R_OK | fs.constants.W_OK, (pubKeyAccessErr) => {
+      const pubKeyExists = !pubKeyAccessErr;
       doForce(keyExists, pubKeyExists);
     });
   });
+
+  /**
+   *
+   * @param {boolean} keyExists
+   * @param {boolean} pubKeyExists
+   */
   function doForce(keyExists, pubKeyExists) {
     if (!force && keyExists) return callback(location + ' already exists');
     if (!force && pubKeyExists) return callback(pubLocation + ' already exists');
     if (!keyExists && !pubKeyExists) return callback();
     if (keyExists) {
       log('removing ' + location);
-      fs.unlink(location, function (err) {
+      fs.unlink(location, (err) => {
         if (err) return callback(err);
         keyExists = false;
         if (!keyExists && !pubKeyExists) callback();
@@ -45,7 +59,7 @@ function checkAvailability(location, force, callback) {
     }
     if (pubKeyExists) {
       log('removing ' + pubLocation);
-      fs.unlink(pubLocation, function (err) {
+      fs.unlink(pubLocation, (err) => {
         if (err) return callback(err);
         pubKeyExists = false;
         if (!keyExists && !pubKeyExists) callback();
@@ -53,16 +67,19 @@ function checkAvailability(location, force, callback) {
     }
   }
 }
-function ssh_keygen(location, opts, callback) {
-  opts || (opts = {});
 
-  var pubLocation = location + '.pub';
-  if (!opts.comment) opts.comment = '';
-  if (!opts.password) opts.password = '';
-  if (!opts.size) opts.size = '2048';
-  if (!opts.format) opts.format = 'RFC4716';
+/**
+ *
+ * @param {string} location
+ * @param { {read:boolean, destroy: boolean, size:string, comment:string, password:string, format:string} } opts
+ * @param {(err?: any, out?: {key:string, pubKey:string}) => void} callback
+ */
+function execSshKeygen(location, opts, callback) {
+  const pubLocation = location + '.pub';
+  const read = opts.read;
+  const destroy = opts.destroy;
 
-  var keygen = spawn(binPath(), [
+  const keygen = spawn(binPath(), [
     '-t',
     'rsa',
     '-b',
@@ -77,74 +94,87 @@ function ssh_keygen(location, opts, callback) {
     opts.format,
   ]);
 
-  keygen.stdout.on('data', function (a) {
-    log('stdout:' + a);
+  keygen.stdout.on('data', (chunk) => {
+    log('stdout:' + chunk);
   });
 
-  var read = opts.read;
-  var destroy = opts.destroy;
-
-  keygen.on('exit', function () {
+  keygen.on('exit', () => {
     log('exited');
-    if (read) {
-      log('reading key ' + location);
-      fs.readFile(location, 'utf8', function (_err, key) {
-        if (destroy) {
-          log('destroying key ' + location);
-          fs.unlink(location, function (err) {
+
+    if (!read) {
+      if (callback) callback();
+      return;
+    }
+
+    log('reading key ' + location);
+    fs.readFile(location, 'utf8', (_err, key) => {
+      if (destroy) {
+        log('destroying key ' + location);
+        fs.unlink(location, (err) => {
+          if (err) return callback(err);
+          readPubKey();
+        });
+      } else {
+        readPubKey();
+      }
+
+      function readPubKey() {
+        log('reading pub key ' + pubLocation);
+        fs.readFile(pubLocation, 'utf8', (_err, pubKey) => {
+          if (!destroy) {
+            callback(undefined, { key: key, pubKey: pubKey });
+            return;
+          }
+
+          log('destroying pub key ' + pubLocation);
+          fs.unlink(pubLocation, (err) => {
             if (err) return callback(err);
-            readPubKey();
+
+            key = key.toString();
+            key = key.substring(0, key.lastIndexOf('\n')).trim();
+            pubKey = pubKey.toString();
+            pubKey = pubKey.substring(0, pubKey.lastIndexOf('\n')).trim();
+            callback(undefined, { key: key, pubKey: pubKey });
           });
-        } else readPubKey();
-        function readPubKey() {
-          log('reading pub key ' + pubLocation);
-          fs.readFile(pubLocation, 'utf8', function (_err, pubKey) {
-            if (destroy) {
-              log('destroying pub key ' + pubLocation);
-              fs.unlink(pubLocation, function (err) {
-                if (err) return callback(err);
-                key = key.toString();
-                key = key.substring(0, key.lastIndexOf('\n')).trim();
-                pubKey = pubKey.toString();
-                pubKey = pubKey.substring(0, pubKey.lastIndexOf('\n')).trim();
-                return callback(undefined, {
-                  key: key,
-                  pubKey: pubKey,
-                });
-              });
-            } else callback(undefined, { key: key, pubKey: pubKey });
-          });
-        }
-      });
-    } else if (callback) callback();
+        });
+      }
+    });
   });
 
-  keygen.stderr.on('data', function (a) {
-    log('stderr:' + a);
+  keygen.stderr.on('data', (chunk) => {
+    log('stderr:' + chunk);
   });
 }
 
-module.exports = function sshKeygen(opts, callback) {
-  var location = opts.location;
-  if (!location) location = path.join(os.tmpdir(), 'id_rsa');
+module.exports = function sshKeygen(opts = {}, callback = undefined) {
+  const location = opts.location || path.join(os.tmpdir(), 'id_rsa');
 
   if (_.isUndefined(opts.read)) opts.read = true;
   if (_.isUndefined(opts.force)) opts.force = true;
   if (_.isUndefined(opts.destroy)) opts.destroy = false;
+  if (!opts.comment) opts.comment = '';
+  if (!opts.password) opts.password = '';
+  if (!opts.size) opts.size = '2048';
+  if (!opts.format) opts.format = 'RFC4716';
 
+  /**
+   * @param {Function} onSuccess
+   * @param {Function} onError
+   */
   function run(onSuccess, onError) {
-    checkAvailability(location, opts.force, function (err) {
+    checkAvailability(location, opts.force, (err) => {
       if (err) {
         log('availability err ' + err);
         onError(err);
-      } else {
-        ssh_keygen(location, opts, onSuccess);
+        return;
       }
+
+      execSshKeygen(location, opts, onSuccess);
     });
   }
 
   if (_.isUndefined(callback)) {
-    var util = require('util');
+    const util = require('util');
     return util.promisify(run)();
   } else {
     run(callback, callback);
