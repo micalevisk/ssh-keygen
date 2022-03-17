29c6a3d358456e9810d2998d9120443c9439d390
Micael Levi (@micalevisk) <mllc@icomp.ufam.edu.br>
2022-03-17 19:09:32 -0400

    refactor: clean up utils module

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,7 @@
 /**
- * Is a given value undefined?
- *
  * (c) https://github.com/jashkenas/underscore/blob/b713f5a6d75b12c8c57fb3f410df029497c2a43f/modules/isUndefined.js
  *
  * @param {any} value
- * @returns {boolean}
+ * @returns {boolean} `true` if `value` is `undefined`
  */
-function isUndefined(value) {
-  return value === void 0;
-}
-
-module.exports.isUndefined = isUndefined;
+module.exports.isUndefined = (value) => value === void 0;
